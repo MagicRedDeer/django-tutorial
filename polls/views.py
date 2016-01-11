b7a50554db54e1a9e5fe1ac788cb7cdead3017ad
Talha Ahmed <talha.ahmed@gmail.com>
2016-01-11 18:24:41 +0500

    part3 - cleanup

--- polls/views.py
+++ polls/views.py
@@ -8,25 +8,10 @@ from .models import Question
 
 # Create your views here.
 
-# def index(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    # context = {
-        # 'latest_question_list': latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-    # try:
-        # question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-        # raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
