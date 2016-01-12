ffc8327d2302b229bd3083121f3690f3f355e37a
Talha Ahmed <talha.ahmed@gmail.com>
2016-01-12 18:37:21 +0500

    part6 - serving static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_FINDER = ["django.contrib.staticfiles.finders.FileSystemFinder",
+        "django.contrib.staticfiles.finders.AppDirectoriesFinder"]
